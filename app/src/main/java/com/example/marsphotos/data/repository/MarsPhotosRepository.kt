c5d2722da473f24afa87c51397ef51daec440f93
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2023-06-13 15:55:40 +0300

    Connect to the internet

---
+++ app/src/main/java/com/example/marsphotos/data/repository/MarsPhotosRepository.kt
@@ -0,0 +1,14 @@
+package com.example.marsphotos.data.repository
+
+import com.example.marsphotos.data.model.MarsPhoto
+import com.example.marsphotos.data.network.MarsApiService
+
+interface MarsPhotosRepository {
+    suspend fun getMarsPhotos(): List<MarsPhoto>
+}
+
+class NetworkMarsPhotosRepository(private val marsApiService: MarsApiService) :
+    MarsPhotosRepository {
+    override suspend fun getMarsPhotos(): List<MarsPhoto> = marsApiService.getPhotos()
+
+}
